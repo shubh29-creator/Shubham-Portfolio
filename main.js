17571bd56f7d82a8edf268d493738e92271b2c05
Shubham Goel <goelshubh29@gmail.com>
2025-12-03 10:21:54 +0530

    Implement dark/light mode toggle feature
    Added dark/light mode toggle functionality.

--- main.js
+++ main.js
@@ -12,3 +12,19 @@ function greet() {
     console.log("Have a great day, Shubham!");
 }
 greet();
+
+
+// ------------------------------
+// DARK / LIGHT MODE TOGGLE
+// ------------------------------
+const modeBtn = document.getElementById("modeToggle");
+
+modeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+        modeBtn.textContent = "Dark Mode";
+    } else {
+        modeBtn.textContent = "Light Mode";
+    }
+});
