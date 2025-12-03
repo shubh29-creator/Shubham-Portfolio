97182a9ef3d464b97ec3a924a6c8274989a21128
Shubham Goel <goelshubh29@gmail.com>
2025-12-03 09:40:56 +0530

    Add main.js for portfolio interactivity

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Portfolio JS Successfully Connected!");
+
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Welcome to Shubham's Portfolio!");
+});
+
+window.addEventListener("scroll", () => {
+    console.log("User is scrolling...");
+});
+
+function greet() {
+    console.log("Have a great day, Shubham!");
+}
+greet();
